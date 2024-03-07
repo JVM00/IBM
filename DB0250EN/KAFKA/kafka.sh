931f40086a81e2a6d98aab2998ad8e4f4227fcee
JVM00 <160590285+JVM00@users.noreply.github.com>
2024-03-07 12:39:56 +0100

    Update kafka.sh

--- DB0250EN/KAFKA/kafka.sh
+++ DB0250EN/KAFKA/kafka.sh
@@ -1,3 +1,13 @@
+python3 -m pip install kafka-python
+python3 -m pip install mysql-connector-python==8.0.31
+
+cd kafka_2.12-3.5.1
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+cd kafka_2.12-3.5.1
+bin/kafka-server-start.sh config/server.properties
+
+
 mysql --host=127.0.0.1 --port=3306 --user=root --password=…..!
 
 #>> create database tolldata;
@@ -8,7 +18,19 @@ mysql --host=127.0.0.1 --port=3306 --user=root --password=…..!
 
 #>> exit
 
-python3 -m pip install kafka-python
-python3 -m pip install mysql-connector-python==8.0.31
+
+sudo systemctl start zookeeper
+sudo systemctl start kafka
+
+cd /usr/local/kafka
+
+bin/kafka-topics.sh --bootstrap-server localhost:9092 --create --topic toll
+bin/kafka-topics.sh --bootstrap-server localhost:9092 --list
+
+python3 toll_traffic_generator.py
 
 python3 streaming_data_reader.py
+
+sudo mysql --host=127.0.0.1 --port=3306 --user=root --password=x!
+#>> USE tolldata;
+#>> select * FROM livetolldata LIMIT 10; 
